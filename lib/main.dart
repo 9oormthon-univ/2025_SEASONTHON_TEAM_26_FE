4be52c2a2f516b3838b6d660a55325504a5d597e
chaeyeon <91270087+zx0501@users.noreply.github.com>
2025-08-30 18:56:55 +0900

    Basic setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
+import 'screens/bus_status_screen.dart';
+import 'screens/bus_application_screen.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '꿈마중 버스',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        fontFamily: 'Pretendard',
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignupScreen(),
+        '/bus-status': (context) => const BusStatusScreen(),
+        '/bus-application': (context) => const BusApplicationScreen(),
+      },
+    );
+  }
+}
 
 
